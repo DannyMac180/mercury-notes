abe46c38bb109d6ef5109b1310168cff87e32ac0
Daniel McAteer <danielmcateer@Daniels-MacBook-Pro.local>
2024-02-04 21:59:37 -0500

    working redirect after login

--- app/lib/firebase-auth.ts
+++ app/lib/firebase-auth.ts
@@ -1,12 +1,9 @@
 import { signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import { auth, googleProvider, githubProvider } from './firebase';
-import { redirect } from 'next/navigation';
 
 const signInWithEmail = async (email: string, password: string) => {
   try {
     const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    
-    redirect('/notes')
   } catch (error) {
     // Handle Errors here.
     console.error('Error signing in with email and password', error);
@@ -17,8 +14,6 @@ const signInWithEmail = async (email: string, password: string) => {
 const signInWithGoogle = async () => {
   try {
     const userCredential = await signInWithPopup(auth, googleProvider);
-    
-    redirect('/notes')
   } catch (error) {
     // Handle Errors here.
     console.error('Error signing in with Google', error);
@@ -29,8 +24,6 @@ const signInWithGoogle = async () => {
 const signInWithGithub = async () => {
   try {
     const userCredential = await signInWithPopup(auth, githubProvider);
-    
-    redirect('/notes')
   } catch (error) {
     // Handle Errors here.
     console.error('Error signing in with GitHub', error);
